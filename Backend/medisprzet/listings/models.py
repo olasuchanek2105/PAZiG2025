920b4808bb130776f3ad6a592eacce18b10e8a91
ogansczyk <oliwia6543217@gmail.com>
2025-04-04 11:30:09 +0200

    baza ordersow

--- Backend/medisprzet/listings/models.py
+++ Backend/medisprzet/listings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Orders(models.Model):
+    order_id = models.Index()
+    user_id = models.IntegerField()
+    category_id = models.IntegerField()
+    title = models.TextField()
+    description = models.TextField()
+    price = models.DecimalField()
+    created_at = models.DateTimeField()
+    address = models.TextField()
+    status = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
